0e12869040956a7fcf15af34208d02e0b8764302
Ricardo Pereira <ricardo.pereira@bounteous.com>
2020-01-31 14:23:14 -0500

    Improving config to skip the Algolia Gridsome plugin either during development builds, or if credentials are missing.

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,13 +4,27 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const moltin = require('./config/moltin');
+const algolia = require('./config/algolia');
+const configureWebpack = require('./config/webpack');
+
+const plugins = [moltin];
+
+// Only load the Algolia plugin:
+if (
+  // 1. during production builds;
+  process.env.NODE_ENV === 'production'
+  // 2. if credentials are in the environment.
+  && ['ALGOLIA_APP_ID', 'ALGOLIA_ADMIN_KEY']
+    .every((key) => typeof process.env[key] === 'string' && process.env[key].length >= 10)
+) {
+  plugins.push(algolia);
+}
+
 module.exports = {
   siteName: 'Bright',
-  configureWebpack: require('./config/webpack'),
-  plugins: [
-    require('./config/moltin'),
-    require('./config/algolia'),
-  ],
+  configureWebpack,
+  plugins,
   templates: {
     MoltinBrand: '/brand/:slug',
     MoltinCategory: '/category/:slug',
